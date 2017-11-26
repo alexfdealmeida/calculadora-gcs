c3903e170f5c1c2fc9088b099b514ca6e3c4d404
saboyafelipe <felipesaboya88@gmail.com>
2017-11-26 16:23:12 -0200

    #1 Adicionada estrutura básica do Projeto Maven.

---
+++ src/main/java/br/com/alexefelipe/calculadora_gcs/App.java
@@ -0,0 +1,13 @@
+package br.com.alexefelipe.calculadora_gcs;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
